a5e67a0a552b0f140588490b71b7d38efbc6b857
piterwifraid-ops <piterwifraid@gmail.com>
2025-12-26 20:07:35 -0200

    Delete src directory

--- src/utils/navigation.ts
+++
@@ -1,6 +0,0 @@
-import { NavigateFunction } from 'react-router-dom';
-
-export const navigateWithUTM = (navigate: NavigateFunction, path: string) => {
-  const utmParams = '?utm_source=FB&utm_campaign={{campaign.name}}|{{campaign.id}}&utm_medium={{adset.name}}|{{adset.id}}&utm_content={{ad.name}}|{{ad.id}}&utm_term={{placement}}';
-  navigate(`${path}${utmParams}`);
-};
